b9f17ba2f92aa44cf37cd898a719641891bcf0be
IndianCoder666 <v.muhlisov@yandex.ru>
2021-03-26 12:49:50 +0300

    Код менеджера ларька

--- Assets/RealMenGame/Scripts/LarekManager.cs
+++ Assets/RealMenGame/Scripts/LarekManager.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using RealMenGame.Scripts.Common;
 using UniRx;
 using UnityEngine;
@@ -6,11 +7,34 @@ namespace RealMenGame.Scripts
 {
     public class LarekManager : MonoBehaviourSingleton<LarekManager>
     {
+        [SerializeField] private Shaurma shaurma;
+        
+        public bool IsShooting { get; private set; }
+
         public ReactiveProperty<int> Health = new ReactiveProperty<int>();
 
-        public void SetDamage()
+
+        public void SetDamage(int damage)
         {
-            
+            Health.Value -= damage;
+        }
+
+        public void OnShoot(Transform target)
+        {
+            Shoot(target).Forget();
+        }
+
+        private async UniTask Shoot(Transform target)
+        {
+            IsShooting = true;
+            await shaurma.FlyTo(target);
+            shaurma.ResetShaurma();
+            IsShooting = false;
+        }
+
+        public void AddIngredient(IngredientType ingredientType)
+        {
+            shaurma.AddIngredient(ingredientType);
         }
     }
 }
