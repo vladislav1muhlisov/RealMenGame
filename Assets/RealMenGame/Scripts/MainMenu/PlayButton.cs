62404c013333a5700f77acff7e3c5e5b98c9ad6e
IndianCoder666 <v.muhlisov@yandex.ru>
2021-03-26 19:17:25 +0300

    Добавлена сцена главного меню

---
+++ Assets/RealMenGame/Scripts/MainMenu/PlayButton.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RealMenGame.Scripts.MainMenu
+{
+    public class PlayButton : MonoBehaviour
+    {
+        [UsedImplicitly]
+        public void OnClick()
+        {
+            SceneManager.LoadScene("Shaurmyachnaya");
+        }
+    }
+}
