b8e9b6f7ce45045bc93585692b4395b734e4dfe1
IndianCoder666 <v.muhlisov@yandex.ru>
2021-03-27 11:49:51 +0300

    Код для звуков кнопок

---
+++ Assets/RealMenGame/Scripts/Sounds/ButtonSound.cs
@@ -0,0 +1,27 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RealMenGame.Scripts.Sounds
+{
+    public class ButtonSound : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+        [SerializeField] private AudioClipSettings _audioClip;
+
+        private void Reset()
+        {
+            _button = GetComponent<Button>();
+        }
+
+        private void Awake()
+        {
+            _button.onClick.AsObservable().Subscribe(OnClick).AddTo(gameObject);
+        }
+
+        private void OnClick(Unit _)
+        {
+            UISound.Instance.Play(_audioClip);
+        }
+    }
+}
---
+++ Assets/RealMenGame/Scripts/Sounds/UISound.cs
@@ -0,0 +1,17 @@
+﻿using RealMenGame.Scripts.Common;
+using UnityEngine;
+
+namespace RealMenGame.Scripts.Sounds
+{
+    public class UISound : MonoBehaviourSceneSingleton<UISound>
+    {
+        [SerializeField] private AudioSource _audioSource;
+
+        public void Play(AudioClipSettings audioClip)
+        {
+            _audioSource.clip = audioClip.AudioClip;
+            _audioSource.volume = audioClip.Volume;
+            _audioSource.Play();
+        }
+    }
+}
